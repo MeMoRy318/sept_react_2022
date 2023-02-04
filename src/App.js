86ebb4fcf9d52d47c0a5c43435d7af77f2cfb7c6
User <girain3181@gmail.com>
2023-02-04 04:33:33 +0300

    Finish

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import {Route, Routes} from "react-router-dom";
 
 const App = () => {
     return (
-        <div>
+        <Routes>
+            <Route>
 
-        </div>
+            </Route>
+        </Routes>
     );
 };
 
-export default App;
+export {App};
