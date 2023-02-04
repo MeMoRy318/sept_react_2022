c57be5bb189b79733f35dc3bb427395619bb15d5
User <girain3181@gmail.com>
2023-02-04 03:43:36 +0300

    Finish

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 
 const App = () => {
+
+    const initValue = () => (
+
+        {count:0}
+    );
+
+    const reducer = (state,action) => {
+
+        switch (action.type) {
+            case 'INC':
+                return {...state,count: state.count += 1}
+            case 'DEC':
+                return {...state,count:state.count -= 1}
+            case 'RES':
+                return {...state,count:state.count = 0}
+            default:
+                return {...state}
+        }
+
+    };
+
+     const [state,dispatch] = useReducer(reducer,null,initValue);
+
     return (
         <div>
-
+            <div>{state.count}</div>
+            <button onClick={()=>dispatch({type:'INC'})}>INCREMENT</button>
+            <button onClick={()=>dispatch({type:'DEC'})}>DECREMENT</button>
+            <button onClick={()=>dispatch({type:'RES'})}>RESET</button>
         </div>
     );
 };
